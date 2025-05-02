486fdddcefd2b82ea121349b8dca43a5dfe0f27c
Marley Alford <marleycalford@gmail.com>
2025-05-02 22:22:25 +0200

    Works. Removed all comments

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,31 @@
 import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
 import "./App.css";
 
 function App() {
   const [input, setInput] = useState("");
-  const [query, setQuery] = useState("");
-  console.log(input);
+  const [weatherData, setWeatherData] = useState(null);
+
+  const searchWeather = (query) => {
+    fetch(
+      `http://api.weatherapi.com/v1/current.json?key=bc3d974648b545299bb110927242810&q=${query}&aqi=no`
+    )
+      .then((res) => res.json())
+      .then((data) => {
+        setWeatherData(data);
+      });
+  };
 
   return (
     <>
       <h1>Weather search</h1>
       <input name="myInput" onChange={(e) => setInput(e.target.value)}></input>
-      <button onClick={() => setQuery(input)}>search</button>
+      <button onClick={() => searchWeather(input)}>search</button>
+      {weatherData && (
+        <p>
+          Weather in {weatherData.location.name}, {weatherData.location.country}{" "}
+          is {weatherData.current.condition.text}
+        </p>
+      )}
     </>
   );
 }
